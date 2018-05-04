200fda1fbe9d3c3f5422be0ea444121032a85f11
Levi DeJean <levidejean@gmail.com>
2018-05-03 21:29:07 -0500

    day-2

--- practice.js
+++ practice.js
@@ -10,54 +10,75 @@
 
 /*
   Create an object called me. 
-  Give it a key of name with the value being your name, and another key of age with the value being your age. 
-  Then alert your name using dot notation.
+  Give it a key of name with the value being your name, and another 
+  //key of age with the value being your age.  Then alert your name 
+  //using dot notation.
 */
 
 //Code here
 
-
-
+var me = {
+  name: "Levi",
+  age: 28,
+}
+console.log(me.name);
 ////////// PROBLEM 2 //////////
 
 /*
-  Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. 
-  Have the values to those keys be your favorite thing in that category.
+  Make a 'favoriteThings' object that contains the following keys: band, 
+  //food, person, book, movie, holiday. Have the values to those keys 
+  //be your favorite thing in that category.
 */
 
 //Code here
 
+var favoriteThings = {
+  band: "Led Zeplin",
+  food: "pho",
+  person: "myself",
+  book: "hunger games",
+  movie: "the wolf of wallstreet",
+  holiday: "christmas"
+}
+
+/*
+  After you've made your object, add another key named 'car' with the 
+  //value being your favorite car and then another key named 'brand' 
+  //with the value being your favorite brand.
+*/
+
+//Code here
+favoriteThings.car = "Lambo";
+favoriteThings.brand = "Ford";
 
 
 /*
-  After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
+  Now change the value of the food key in your favoriteThings object to 
+  //be 'Chicken Nuggets' and change the value of the book key in your 
+  //favoriteThings object to be 'Harry Potter'.
 */
 
 //Code here
 
-
-
-/*
-  Now change the value of the food key in your favoriteThings object to be 'Chicken Nuggets' and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
-*/
-
-//Code here
-
-
+favoriteThings.food = "Chicken Nuggets";
+favoriteThings.book= "Harry Potter";
 
 ////////// PROBLEM 3 //////////
 
 /* 
   Create an empty Object called backPack. 
-  Now, create a variable called 'item' and set it equal to the string 'firstPocket'. 
-  Using bracket notation, add a 'firstPocket' key (or property) to backPack, using 'item'.
-  Set the value of that key to 'chapstick'.
-  Using dot notation, add another key (or property) to your backPack object that is named color, with the value being the color of your backpack. 
+  Now, create a variable called 'item' and set it equal to the string 
+  //'firstPocket'. Using bracket notation, add a 'firstPocket' key 
+  //(or property) to backPack, using 'item'. Set the value of that key
+  // to 'chapstick'. Using dot notation, add another key (or property) 
+  //to your backPack object that is named color, with the value being 
+  //the color of your backpack. 
 */
 
 //Code here
 
-
+var backPack = {};
+var item = "firstPocket" 
 
 /*
   After you do the above, alert your entire backPack object.
@@ -65,12 +86,13 @@
 
 //Code here
 
-
+backPack[item] = "chapstick";
 
 /*
 You probably noticed that it just alerted [object Object].
 Alerting to see the data in your Object doesn't work so well.
-Instead, console.log your whole backPack object and then check out the console. 
+Instead, console.log your whole backPack object and then check out the 
+//console. 
 */
 
 //Code here
@@ -91,13 +113,14 @@ var user2 = {
 // Do not edit the code above.
 
 /* 
-  Let's say I, the user, decided to change my name and email address to the following:
-  name -> 'Tyler S. McGinnis' and email -> 'tyler.mcginnis@devmounta.in'. 
+  Let's say I, the user, decided to change my name and email address 
+  //to the following: name -> 'Tyler S. McGinnis' and email -> 'tyler.mcginnis@devmounta.in'. 
   Make that change without modifying the original object code above.
 */
 
 //Code Here
-
+user2.name = 'Tyler S. McGinnis';
+user2.email = 'tyler.mcginnis@devmounta.in';
 
 
 ////////// EXTRA PRACTICE PROBLEMS BELOW //////////
@@ -110,17 +133,23 @@ var user2 = {
 
 //Code Here
 
-
+var methodCollection = {};
 
 /*
-  Now add two methods (functions that are properties on objects) to your methodCollection object. 
-  One called 'alertHello' which alerts 'hello' and another method called 'logHello' which logs 'hello' to the console. 
+  Now add two methods (functions that are properties on objects) to 
+  //your methodCollection object. One called 'alertHello' which alerts 
+  //'hello' and another method called 'logHello' which logs 'hello' to
+  // the console. 
 */
 
 //Code Here
 
-
-
+methodCollection.alertHello = function(){
+  alert("hello");
+};
+methodCollection.logHello = function(){
+  console.log("hello");
+};
 /*
   Now call your alertHello and logHello methods.
 */
@@ -132,21 +161,37 @@ var user2 = {
 ////////// PROBLEM 6 //////////
 
 /* 
-  Create a function called makePerson which takes in name, birthday, ssn as its parameters. 
-  Return a new object with all of the information that you passed in.
+  Create a function called makePerson which takes in name, birthday, 
+  //ssn as its parameters. Return a new object with all of the 
+  //information that you passed in.
 */
 
 //Code Here
 
-
+function makePerson(nameParam, birthdayParam, ssnParam){
+  return {
+    name: nameParam,
+    birthday: birthdayParam,
+    ssn: ssnParam
+  }
+}
+var newPerson = makePerson("Levi", "Aug 13", 123456789)
 
 ////////// PROBLEM 7 //////////
 
 /*
-  Create a function called makeCard which takes in cardNumber, expirationDate, and securityCode to make a Credit Card object.
-  Return that object so that whenever you invoke makeCard, you get a brand new credit card.
+  Create a function called makeCard which takes in cardNumber, 
+  //expirationDate, and securityCode to make a Credit Card object.
+  Return that object so that whenever you invoke makeCard, you get a 
+  //brand new credit card.
 */
 
 //Code Here
 
-
+function makeCard (cardNumber, expirationDate, securityCode){
+  return{
+  number: cardNumber,
+  experation: expirationDate,
+  code: securityCode
+  }
+};
